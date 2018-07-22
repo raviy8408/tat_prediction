805eef496914fe748588a883597d18c0811ee30b
Ravi Yadav <raviy8408@gmail.com>
2018-07-22 17:57:16 +0530

    EDA completed

--- eda.py
+++ eda.py
@@ -1,9 +1,18 @@
 from support_func import *
 from dateutil import parser
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+# directories
+
+data_dir = "/Users/Ravi/Desktop/modeling/tat_prediction/"
+image_dir = "/Users/Ravi/Desktop/modeling/tat_prediction/plots/"
 
 # load data
 
-data = load_data("/Users/Ravi/Desktop/modeling/tat_prediction/train_data.csv")
+data = load_data(data_dir + "train_data.csv")
 
 print("\ndata head:\n")
 print(data.head())
@@ -23,14 +32,43 @@ cat_col = ['mobmake', 'hubid', 'hubpincode', 'pickuppartnerid', 'deliverypartner
 
 date_col = ['memcreateddate', 'mobilepurchasedate', 'claimintdate', 'pickupStartdate']
 
+outcome_col = 'tat_in_days'
+
 data[int_col] = data[int_col].apply(lambda x: x.astype(int))
-
 data[cat_col] = data[cat_col].apply(lambda x: x.astype('category'))
-
 data[date_col] = data[date_col].apply(lambda x: x.apply(parser.parse))
+
+data = data.drop(['hubpincode'], axis= 1) # duplicate column as hubid
+cat_col.remove('hubpincode')
+
+print("\ntransformed data head:\n")
+print(data.head())
 
 print("\ncolumn types after transformation:\n")
 print(data.dtypes)
 
+print("\nnumber of data points:")
+print(len(data))
+
+print("\ndata summary:\n")
+print(data.describe(include= 'all'))
+
+print("\ncount of nan values:\n")
+print(data.isnull().sum())
+
 print("#########################################\n")
 
+
+# UVA
+
+# outcome variable
+
+save_hist(data=data['tat_in_days'],bins=100,alpha=0.7,image_dir=image_dir)
+
+# outcome variable vs date type cols
+
+data[date_col].apply(lambda x: save_scatter(x= x, y= data[outcome_col], image_dir= image_dir))
+
+# outcome variables vs categorical data
+
+save_boxPlots_for_cat_var(data= data, cat_var_list= cat_col, y = outcome_col, image_dir= image_dir)
--- support_func.py
+++ support_func.py
@@ -1,7 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+from datetime import datetime
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+
+
+rcParams['figure.figsize'] = 15, 6
+
+
 def load_data(file_location):
-
-    import pandas as pd
-
-    data = pd.read_csv(file_location, sep= ",", header=0)
+    data = pd.read_csv(file_location, sep=",", header=0)
 
     return data
+
+
+def save_hist(data, bins, alpha, image_dir, color='green'):
+    fig = plt.figure()
+    ax = plt.subplot()
+    plt.hist(data, bins=bins, color=color, alpha=alpha)
+    ax.set_xlabel(data.name)
+    plt.title(data.name)
+    plt.savefig(image_dir + data.name + '.png', bbox_inches='tight')
+    plt.close(fig)
+
+
+def save_scatter(x, y, image_dir):
+    """
+    saves scatter plot for given pandas series as x and y
+    :param x: pd series
+    :param y: pd series
+    :param image_dir: file save location
+    :return: none
+    """
+    df = pd.DataFrame()
+    df[str(x.name)] = x.astype(np.int64)
+    df[str(y.name)] = y
+    fig = plt.figure()
+    ax = plt.subplot()
+    df.plot(x=str(x.name), y=str(y.name), kind='scatter', ax=ax)
+    ax.set_xlabel(x.name)
+    ax.set_ylabel(y.name)
+
+    ax.set_xticklabels([datetime.fromtimestamp(ts / 1e9).strftime('%Y-%m-%d') for ts in ax.get_xticks()])
+    plt.title(y.name + " vs " + x.name)
+    plt.savefig(image_dir + y.name + "_vs_" + x.name + '.png', bbox_inches='tight')
+    plt.close(fig)
+
+def save_boxPlots_for_cat_var(data, cat_var_list, y, image_dir):
+
+    for elem in cat_var_list:
+
+        if len(data[elem].unique()) <= 30:
+            dfg = data.groupby(elem)
+
+            counts = [len(v) for k, v in dfg]
+            total = float(sum(counts))
+            cases = len(counts)
+
+            widths = [c / total for c in counts]
+
+            fig = plt.figure()
+            # ax = plt.subplot()
+            cax = data[[elem, y]].boxplot(by= elem)
+            cax.set_xticklabels(['%s\n$n$=%d' % (k, len(v)) for k, v in dfg])
+            plt.savefig(image_dir + y + "_vs_" + elem + ".png", bbox_inches='tight')
+            plt.close(fig)
+
